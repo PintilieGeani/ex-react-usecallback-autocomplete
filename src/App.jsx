95b8438c573998fe27b47368324e5ea438756bd2
PintilieGeani <pintilie.geany@yahoo.it>
2025-08-29 20:17:58 +0200

    feat: milestone-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import {useState, useEffect, useCallback} from "react"
+import { useState, useEffect, useCallback } from "react"
+
+function debounce(callBack, delay) {
+  let timer;
+  return (value) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      callBack(value)
+    }, delay)
+  }
+}
 
 function App() {
   const [query, setQuery] = useState("")
-  const apiUrl = `http://localhost:3333/products?search=${query}`
+
   const apiUrlProva = `http://localhost:3333/products`
-  
-  const fetchJson = async (url) =>{
+
+  const fetchJson = async (url) => {
     const response = await fetch(url)
     const data = await response.json()
 
@@ -13,38 +23,46 @@ function App() {
   }
 
   const [products, setProducts] = useState([])
-  
+  const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
-    const fetchApi = async () =>{
+  const eseguiFetch = debounce((query) => {
+    console.log("eseguo la query:", query)
+    const fetchApi = async () => {
+      const apiUrl = `http://localhost:3333/products?search=${query}`
       const data = await fetchJson(apiUrl)
       setProducts(data)
-    } 
+    }
     fetchApi()
-  }, [query])
+  }, 500)
 
-  console.log(`eseguo la query: ${query}`, products)
+
+
+  const eseguiFetchCallBack = useCallback(eseguiFetch, [])
+
+  useEffect(() => {
+    eseguiFetchCallBack(query)
+  }, [query, eseguiFetchCallBack])
+
   
-
   return (
     <>
-    <div className="inpaginazione">
-      <div className="input">
-        <input 
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        />
-      </div>
-      {query.length > 0 && <div className="card-container">
-        <div className="card">
-          {products.map((cur) => (
-            <p>{cur.name}</p>
-          ))}
+      <div className="inpaginazione">
+        <div className="input">
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
         </div>
-      </div>}
-      
-    </div>
+        {query.length > 0 && <div className="card-container">
+          <div className="card">
+            {products.map((cur) => (
+              <p>{cur.name}</p>
+            ))}
+          </div>
+        </div>}
+
+      </div>
     </>
   )
 }
